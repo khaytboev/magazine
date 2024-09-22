f96eb88ddd868b49f937ae5d25e7c47ea5e87782
khaytboev <furypubgme@gmail.com>
2024-09-22 21:27:22 +0500

    online magazine

---
+++ 1.js
@@ -0,0 +1,4 @@
+import {p as s} from "./DIDzDSBz.js";
+const o = s("/img/arrow-left.svg")
+  , t = s("/img/arrow-right.svg");
+export {o as _, t as a};
---
+++ 2.js
@@ -0,0 +1,522 @@
+const __vite__mapDeps = (i, m=__vite__mapDeps, d=(m.f || (m.f = ["./DIDzDSBz.js", "./DoqiT5Lj.js", "./swiper-vue.BV_8Ey50.css"]))) => i.map(i => d[i]);
+import {p as he, m as N, b as $, o as _e, c as ve, q as me, a as ge} from "./DIDzDSBz.js";
+import pe from "./CCnTaFCo.js";
+import {_ as ke} from "./d8jPmOFo.js";
+import {x as ee, a as c, b as R, p as fe, K as we, o as n, c as l, g as e, u as a, F as v, d as m, e as be, h as Y, t as w, m as te, i as x, q as L, n as u, l as V, v as ye} from "./DoqiT5Lj.js";
+const Ce = he("/svg/add.svg")
+  , xe = ye( () => ge( () => import("./DIDzDSBz.js").then(g => g.C), __vite__mapDeps([0, 1, 2]), import.meta.url).then(g => g.default || g))
+  , Le = {
+    key: 0,
+    class: "container"
+}
+  , Me = {
+    class: "onmalika-header-top justify-content-between"
+}
+  , Be = {
+    class: "onmalika-header-top-links"
+}
+  , Se = {
+    class: "onmalika-header-top-currency d-flex"
+}
+  , Ie = {
+    class: "d-flex align-items-center column-gap-3"
+}
+  , Ee = e("p", null, "Показывать цены на сайте:", -1)
+  , ze = {
+    class: "onmalika-header-top-currency-toggle",
+    style: {
+        position: "relative",
+        height: "30px"
+    }
+}
+  , je = {
+    class: "onmalika-social"
+}
+  , He = ["href"]
+  , Pe = ["src"]
+  , Ue = e("hr", null, null, -1)
+  , Ne = {
+    class: "container"
+}
+  , $e = {
+    class: "navbar onmalika-header-bottom"
+}
+  , Re = {
+    class: "onmalika-header-bottom-menu d-flex"
+}
+  , Ye = {
+    class: "onmalika-header-bottom-logo"
+}
+  , Ve = e("img", {
+    src: ke,
+    width: "180",
+    height: "30",
+    alt: ""
+}, null, -1)
+  , Ae = e("path", {
+    d: "M3 7H21",
+    stroke: "white",
+    "stroke-width": "1.5",
+    "stroke-linecap": "round"
+}, null, -1)
+  , De = e("path", {
+    d: "M3 12H21",
+    stroke: "white",
+    "stroke-width": "1.5",
+    "stroke-linecap": "round"
+}, null, -1)
+  , Oe = e("path", {
+    d: "M3 17H21",
+    stroke: "white",
+    "stroke-width": "1.5",
+    "stroke-linecap": "round"
+}, null, -1)
+  , qe = [Ae, De, Oe]
+  , Fe = e("path", {
+    d: "M5 19L19 5",
+    stroke: "black",
+    fill: "currentcolor",
+    "stroke-width": "1.5",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+}, null, -1)
+  , Te = e("path", {
+    d: "M19 19L5 5",
+    stroke: "black",
+    fill: "currentcolor",
+    "stroke-width": "1.5",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+}, null, -1)
+  , Ze = [Fe, Te]
+  , Je = {
+    class: "onmalika-header-bottom-user"
+}
+  , Ke = e("button", {
+    type: "button",
+    class: "dark-cover onmalika-header-bottom-user-search",
+    "data-bs-toggle": "modal",
+    "data-bs-target": "#searchModal"
+}, [e("svg", {
+    width: "24",
+    height: "24",
+    viewBox: "0 0 24 24",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg"
+}, [e("path", {
+    d: "M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z",
+    stroke: "",
+    "stroke-width": "1.5",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+}), e("path", {
+    d: "M22 22L20 20",
+    stroke: "",
+    "stroke-width": "1.5",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+})])], -1)
+  , We = {
+    class: "dark-cover onmalika-header-bottom-user-nav"
+}
+  , Ge = ["href"]
+  , Qe = {
+    class: "onmalika-header-top-currency d-flex"
+}
+  , Xe = {
+    class: "d-flex align-items-center column-gap-3"
+}
+  , et = {
+    class: "onmalika-header-top-currency-toggle",
+    style: {
+        position: "relative",
+        height: "30px"
+    }
+}
+  , tt = e("hr", null, null, -1)
+  , ot = {
+    class: "container"
+}
+  , st = {
+    class: "row justify-content-between"
+}
+  , at = {
+    class: "col-3"
+}
+  , nt = {
+    class: "nav onmalika-category-block-list nav-pills mb-3",
+    id: "pills-tab",
+    role: "tablist"
+}
+  , lt = ["onClick", "id", "data-bs-target", "aria-controls"]
+  , rt = e("svg", {
+    width: "100%",
+    height: "24",
+    class: "icon",
+    viewBox: "0 0 24 24",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg"
+}, [e("path", {
+    d: "M11.9199 22C17.4199 22 21.9199 17.5 21.9199 12C21.9199 6.5 17.4199 2 11.9199 2C6.41992 2 1.91992 6.5 1.91992 12C1.91992 17.5 6.41992 22 11.9199 22Z",
+    stroke: "black",
+    "stroke-width": "1.5",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+}), e("path", {
+    d: "M7.91992 12H15.9199",
+    stroke: "black",
+    "stroke-width": "1.5",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+})], -1)
+  , it = e("svg", {
+    width: "100%",
+    height: "14",
+    class: "arrow-icon",
+    viewBox: "0 0 14 14",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg"
+}, [e("path", {
+    d: "M5.19727 11.62L9.0006 7.81667C9.44977 7.36751 9.44977 6.63251 9.0006 6.18334L5.19727 2.38",
+    stroke: "black",
+    "stroke-width": "1.5",
+    "stroke-miterlimit": "10",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+})], -1)
+  , ct = {
+    class: "col-8"
+}
+  , dt = {
+    class: "tab-content onmalika-category-block-subcats",
+    id: "pills-tabContent"
+}
+  , ut = ["id"]
+  , pt = {
+    __name: "Header",
+    props: {
+        catHeader: Object,
+        acf: {
+            required: !1
+        }
+    },
+    async setup(g) {
+        let d, p;
+        const oe = g
+          , {acf: E, catHeader: se} = oe;
+        N("logo", E);
+        const z = N("phone", E)
+          , A = N("social", E)
+          , D = ee( () => se.data.value)
+          , h = c([])
+          , O = t => {
+            const r = {};
+            return t.results.forEach(s => {
+                s.subcategories = [],
+                r[s.id] = s
+            }
+            ),
+            t.results.forEach(s => {
+                s.parent && r.hasOwnProperty(s.parent.id) && r[s.parent.id].subcategories.push(s)
+            }
+            ),
+            t.results.filter(s => s.parent === null)
+        }
+        ;
+        async function q(t, r) {
+            try {
+                const s = await $(`/categories/?parent=${t}`);
+                if (s && s.data && s.data.value && s.data.value.results) {
+                    const y = s.data.value.results;
+                    h.value[r].subcategories = y.filter(C => C.parent && C.parent.id === t),
+                    h.value[r].subcategoriesLoaded = !0
+                } else
+                    console.error("Полученный ответ не содержит ожидаемых данных:", s)
+            } catch (s) {
+                console.error("Ошибка при загрузке подкатегорий:", s)
+            }
+        }
+        const j = c(null)
+          , H = c(null)
+          , _ = c(!0)
+          , F = t => {
+            j.value && !j.value.contains(t.target) && H.value && !H.value.contains(t.target) && (_.value = !0)
+        }
+          , ae = () => {
+            _.value = !0
+        }
+          , ne = () => {
+            _.value = !_.value
+        }
+          , M = c(!1);
+        let T = 0;
+        const Z = () => {
+            const t = window.scrollY;
+            t > 200 && (M.value = !0),
+            t < T && t > 200 && (M.value = !1),
+            t === 0 && (M.value = !1),
+            T = t
+        }
+          , le = async () => {
+            await new Promise(t => setTimeout(t, 500)),
+            h.value = O(D.value)
+        }
+        ;
+        [d,p] = R( () => le()),
+        await d,
+        p();
+        const P = c(!0);
+        fe( () => {
+            h.value = O(D.value),
+            h.value.length > 0 && setTimeout( () => {
+                q(h.value[0].id, 0)
+            }
+            , 500),
+            window.addEventListener("click", F),
+            window.addEventListener("click", G),
+            window.addEventListener("scroll", Z),
+            window.innerWidth < 991 && (P.value = !1)
+        }
+        ),
+        we( () => {
+            window.removeEventListener("click", F),
+            window.removeEventListener("click", G),
+            window.removeEventListener("scroll", Z)
+        }
+        );
+        const {data: b} = ([d,p] = R( () => $("/currency/")),
+        d = await d,
+        p(),
+        d)
+          , k = _e();
+        k.setCurrencies(b.value),
+        k.setSelectedCurrency(k.selectedCurrency);
+        const J = b.value.findIndex(t => t.code === "UZS")
+          , f = c(J !== -1 ? J : 0);
+        function re(t) {
+            f.value = t;
+            const r = b.value[t];
+            k.setSelectedCurrency(r.code)
+        }
+        const K = ee( () => b.value.map( (t, r) => r === f.value ? {
+            transform: "translateY(0)",
+            transition: "transform 0.5s",
+            position: "absolute",
+            left: "0",
+            bottom: "0",
+            display: "flex",
+            top: "0",
+            right: 0,
+            zIndex: 2
+        } : r < f.value ? {
+            transform: "translateY(-100%)",
+            transition: "transform 0.5s",
+            position: "absolute",
+            left: "0",
+            bottom: "0",
+            display: "flex",
+            top: "0",
+            right: 0,
+            zIndex: 1
+        } : {
+            transform: "translateY(100%)",
+            transition: "transform 0.5s",
+            position: "absolute",
+            left: "0",
+            bottom: "0",
+            display: "flex",
+            top: "0",
+            right: 0,
+            zIndex: 1
+        }))
+          , W = () => {
+            f.value = (f.value + 1) % b.value.length,
+            re(f.value)
+        }
+          , U = c(!1)
+          , B = c(!1)
+          , S = c(!1)
+          , G = t => {
+            const r = document.getElementById("modal-nav-links");
+            U.value && !U.value.contains(t.target) && !r.contains(t.target) && (B.value = !1,
+            S.value = !1)
+        }
+          , ie = () => {
+            B.value = !B.value,
+            S.value = !S.value
+        }
+          , [ce] = ([d,p] = R( () => Promise.all([$("/menus/?slug=pages")])),
+        d = await d,
+        p(),
+        d)
+          , de = ce.data
+          , Q = c("https://onmalika.uz");
+        function X(t, r) {
+            if (!t || !r)
+                return;
+            const s = new RegExp(r,"g")
+              , y = s.test(t);
+            return {
+                cleanUrl: t.replace(s, ""),
+                edited: y
+            }
+        }
+        return (t, r) => {
+            const s = ve
+              , y = me
+              , C = pe
+              , ue = xe;
+            return n(),
+            l(v, null, [e("header", {
+                class: u(["onmalika-header", {
+                    "header-transform": a(M),
+                    zbig: a(S)
+                }])
+            }, [a(P) ? (n(),
+            l("div", Le, [e("div", Me, [e("div", Be, [(n(!0),
+            l(v, null, m(a(de)[0].block[0].value.list[0].value, o => (n(),
+            be(s, {
+                to: X(o.url, a(Q)) && X(o.url, a(Q)).cleanUrl || o.url,
+                key: o
+            }, {
+                default: Y( () => [e("span", null, w(o.title), 1)]),
+                _: 2
+            }, 1032, ["to"]))), 128))]), e("div", Se, [e("div", Ie, [Ee, e("div", ze, [(n(!0),
+            l(v, null, m(a(k).currencies, (o, i) => (n(),
+            l("span", {
+                key: o.id,
+                style: te(a(K)[i]),
+                onClick: W
+            }, w(o.symbol), 5))), 128))])]), e("div", je, [a(A) ? (n(!0),
+            l(v, {
+                key: 0
+            }, m(a(A), o => (n(),
+            l("a", {
+                key: o.id,
+                href: o.value.link.custom_url,
+                class: "",
+                target: "_blank"
+            }, [e("img", {
+                src: o.value.icon.url,
+                width: "20",
+                height: "20",
+                alt: ""
+            }, null, 8, Pe)], 8, He))), 128)) : x("", !0)])])])])) : x("", !0), Ue, e("div", Ne, [e("nav", $e, [e("div", Re, [e("div", Ye, [L(s, {
+                to: "/"
+            }, {
+                default: Y( () => [Ve]),
+                _: 1
+            }), e("button", {
+                class: u(["dark-cover in-element", {
+                    active: !a(_)
+                }]),
+                type: "button",
+                ref_key: "toggleButtonRef",
+                ref: H,
+                onClick: ne
+            }, [V(" Каталог "), (n(),
+            l("svg", {
+                class: u({
+                    "d-none": !a(_)
+                }),
+                width: "24",
+                height: "24",
+                viewBox: "0 0 24 24",
+                fill: "none",
+                xmlns: "http://www.w3.org/2000/svg"
+            }, qe, 2)), (n(),
+            l("svg", {
+                class: u({
+                    "d-none": a(_)
+                }),
+                width: "24",
+                height: "24",
+                viewBox: "0 0 24 24",
+                fill: "none",
+                xmlns: "http://www.w3.org/2000/svg"
+            }, Ze, 2))], 2)])]), e("div", Je, [Ke, e("div", We, [a(z) ? (n(),
+            l("a", {
+                key: 0,
+                class: "onmalika-header-bottom-tel",
+                href: `tel:${a(z).link.phone}`
+            }, [e("b", null, w(a(z).text), 1)], 8, Ge)) : x("", !0), L(y)]), e("div", Qe, [e("div", Xe, [e("div", et, [(n(!0),
+            l(v, null, m(a(k).currencies, (o, i) => (n(),
+            l("span", {
+                key: o.id,
+                style: te(a(K)[i]),
+                onClick: W
+            }, w(o.code), 5))), 128))])])]), e("div", {
+                class: "dark-cover onmalika-mobile-navigation-links",
+                ref_key: "isImageChanged",
+                ref: U,
+                onClick: ie,
+                "data-bs-toggle": "modal",
+                "data-bs-target": "#modal-nav-links"
+            }, [e("img", {
+                class: u({
+                    rotate: a(B)
+                }),
+                src: Ce,
+                alt: ""
+            }, null, 2)], 512)])])]), tt, a(P) ? (n(),
+            l("div", {
+                key: 1,
+                class: u(["onmalika-category", {
+                    "d-block": !a(_)
+                }])
+            }, [e("div", ot, [e("div", {
+                ref_key: "categoryBlockRef",
+                ref: j,
+                class: "onmalika-category-block"
+            }, [e("div", st, [e("div", at, [e("ul", nt, [(n(!0),
+            l(v, null, m(a(h), (o, i) => (n(),
+            l("li", {
+                key: o.id,
+                class: "nav-item",
+                role: "presentation"
+            }, [e("button", {
+                onClick: I => q(o.id, i),
+                class: u(["nav-link", {
+                    active: i == 0
+                }]),
+                id: "pills-tab-" + i,
+                "data-bs-toggle": "pill",
+                "data-bs-target": "#pills-content-" + i,
+                type: "button",
+                role: "tab",
+                "aria-controls": "pills-content-" + i,
+                "aria-selected": "true"
+            }, [e("span", null, [rt, V(" " + w(o.name), 1)]), it], 10, lt)]))), 128))])]), e("div", ct, [e("div", dt, [(n(!0),
+            l(v, null, m(a(h), (o, i) => (n(),
+            l("div", {
+                key: o.id,
+                class: u(["tab-pane fade", {
+                    "show active": i == 0
+                }]),
+                id: "pills-content-" + i
+            }, [o && o.subcategoriesLoaded && o.subcategories && o.subcategories.length > 0 ? (n(),
+            l("ul", {
+                key: 0,
+                class: u(["list-menu", {
+                    row: o.subcategories.length > 7
+                }])
+            }, [(n(!0),
+            l(v, null, m(o.subcategories, (I, ht) => (n(),
+            l("li", {
+                class: u({
+                    "col-6": o.subcategories.length > 7
+                }),
+                key: o.id
+            }, [L(s, {
+                onClick: ae,
+                to: `/shop/${I.parent.slug}/${I.slug}`
+            }, {
+                default: Y( () => [V(w(I.name), 1)]),
+                _: 2
+            }, 1032, ["to"])], 2))), 128))], 2)) : x("", !0)], 10, ut))), 128))])])])], 512)])], 2)) : x("", !0)], 2), L(C), L(ue, {
+                data: a(h)
+            }, null, 8, ["data"])], 64)
+        }
+    }
+};
+export {pt as default};
